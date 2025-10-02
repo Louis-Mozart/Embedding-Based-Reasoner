e9dc2b0f7367af2d41f68e6a03266cb9b6b6fce4
Louis Mozart KAMDEM TEYOU <93260304+Louis-Mozart@users.noreply.github.com>
2025-10-02 12:29:06 +0200

    Update concept_learning_evaluation.py

--- examples/concept_learning_evaluation.py
+++ examples/concept_learning_evaluation.py
@@ -2,7 +2,7 @@
 Fitting OWL Class Expression Learners:
 
 Given positive examples (E^+)  and negative examples (E^-),
-Evaluate the performances of OWL Class Expression Learners  w.r.t. the quality of learned/found OWL Class Expression
+Evaluate the performances of OWL Class Expression Learners  w.r.t to the quality of learned/found OWL Class Expression
 
 Example to run the script
 python examples/concept_learning_evaluation.py --lps LPs/Family/lps.json --kb KGs/Family/family-benchmark_rich_background.owl --max_runtime 3 --report family.csv
